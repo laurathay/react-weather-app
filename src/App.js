afbc9af8c723b25e3db5ed005ab4d78754ab9db4
Laura THAY <laura.thay@ipag.fr>
2022-07-22 18:13:27 +0200

    [delete] useless files and add api key

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+const api = {
+  key: "aff798c8891f14eaae40e9c34d95c709",
+  base: "https://api/openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
     </div>
   );
 }
