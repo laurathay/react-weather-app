ce4d69dd896a842809c43f4c12e390aa3c765e72
Laura THAY <laura.thay@ipag.fr>
2022-07-24 21:03:53 +0200

    ajout data

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ import React from 'react'
 // }
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "Aprile", "May", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre" ];
+    let days = ["Sunday", "Monday", "Tuesay", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = dates[d.getDate()];
+  }
+
   return (
     <div className="app">
     <main>
@@ -15,6 +24,10 @@ function App() {
           className="search-bar"
           placeholder="Search..." 
           />
+      </div>
+      <div className="location-box">
+        <div className="location">New York City, US</div>
+        <div className="date">{dateBuilder(new Date())}</div>
       </div>
     </main>
  
