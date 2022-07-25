c4b135a1000089a561827ce60a70772b91129ed9
Laura THAY <laura.thay@ipag.fr>
2022-07-25 20:23:36 +0200

    add location box

--- src/App.js
+++ src/App.js
@@ -33,6 +33,12 @@ function App() {
         <div className="location">New York City, US</div>
         <div className="date">{dateBuilder(new Date())}</div>
       </div>
+      <div className="weather-box">
+        <div className="temp">
+          15C
+        </div>
+        <div className="weather">Sunny</div>
+      </div>
     </main>
  
     </div>
